c1b88de802736b08c575fbb46484f8e2d330456b
mihikasharmaaa <mihikasharma2002@gmail.com>
2019-12-07 18:48:44 -0800

    changed footer so that links can hover

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links. Mihika Sharma, Period 2.</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="Last_AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="Last_WhatWe'veDone.html">What We've Done.html</a></li></br>
+      <li><a href="Last_Inspiration.html">Inspiration.html</a></li></br>
+      <li><a href="Last_ContactMe.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
